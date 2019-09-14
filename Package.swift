04e7a67bb4043513eb1851539aa779e886c3fac2
Александр Шипин <ssanek212@gmail.com>
2019-09-14 22:16:01 +0700

    create library

--- Package.swift
+++ Package.swift
@@ -5,13 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "TelegramAPI",
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        //.package(url: "https://github.com/vapor/vapor.git", from: "3.0.0")
+    products: [
+        .library(name: "TelegramAPI", targets: ["TelegramAPI"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "TelegramAPI",
             dependencies: [])
--- Sources/TelegramAPI/main.swift
+++
@@ -1,8 +0,0 @@
-//
-//  main.swift
-//  TelegramAPI
-//
-//  Created by Alex Shipin on 11.08.2019.
-//  Copyright © 2019 Alexander Shipin. All rights reserved.
-//
-
